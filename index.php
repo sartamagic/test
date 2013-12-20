ab6f12b86c02f662344029fc382f5a6550ce7fb7
Disarta <disarta@gmail.com>
2013-12-20 18:22:48 +0400

    First commit - Index page

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="author" content="SartaMagic" />
+
+	<title>Лабораторная №2</title>
+</head>
+<body>
+
+
+
+</body>
+</html>
